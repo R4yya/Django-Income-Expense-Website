87681f68763871ee77498f336f83d3482528632d
R4yya <121268707+R4yya@users.noreply.github.com>
2023-09-27 22:18:56 +0300

    Update settings.py

--- expenseswebsite/expenseswebsite/settings.py
+++ expenseswebsite/expenseswebsite/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'expenseswebsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'income-expenses-db',
+        'USER': 'postgres',
+        'PASSWORD': 'AHfGx9_rNaXgyAYZr',
+        'HOST': 'localhost'
     }
 }
 
