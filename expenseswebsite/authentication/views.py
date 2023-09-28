4454ea1d9f7c1200f0fc3e942e04933c6929758e
R4yya <121268707+R4yya@users.noreply.github.com>
2023-09-28 23:51:00 +0300

    Update views.py

--- expenseswebsite/authentication/views.py
+++ expenseswebsite/authentication/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.views import View
+from django.http import JsonResponse
+from django.contrib.auth.models import User
+
+import json
 
 
 class RegistrationView(View):
@@ -8,3 +12,19 @@ class RegistrationView(View):
 
     def get(self, request):
         return render(request, 'authentication/register.html')
+
+
+class UsernameValidationView(View):
+    def __init__(self):
+        pass
+
+    def post(self, request):
+        data = json.loads(request.body)
+        username = data['username']
+
+        if not str(username).isalnum():
+            return JsonResponse({'username_error': 'Username should only contain alphanumeric characters'}, status=400)
+        if User.object.filter(username=username).exists():
+            return JsonResponse({'username_error': 'Username is alreaady exists'}, status=409)
+        else:
+            return JsonResponse({'username_valid': True})
