cfb30e872b41a93c9a9fbe00fb825a88d7ec1f3c
R4yya <121268707+R4yya@users.noreply.github.com>
2023-09-28 16:16:17 +0300

    Update views.py

--- expenseswebsite/expenses/views.py
+++ expenseswebsite/expenses/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'expenses/index.html')
+
+
+def add_expense(request):
+    return render(request, 'expenses/add_expense.html')
