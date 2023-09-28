8002294d502621a09c968ccf8c9f195521dc6425
R4yya <121268707+R4yya@users.noreply.github.com>
2023-09-28 16:16:02 +0300

    Update urls.py

--- expenseswebsite/expenses/urls.py
+++ expenseswebsite/expenses/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='expenses')
+    path('', views.index, name='expenses'),
+    path('add-expense', views.add_expense, name='add-expense')
 ]
