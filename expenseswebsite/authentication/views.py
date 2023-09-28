cb5039644e55ef89053c3878d8fde3bd680d3d06
R4yya <121268707+R4yya@users.noreply.github.com>
2023-09-28 18:46:07 +0300

    Update views.py

--- expenseswebsite/authentication/views.py
+++ expenseswebsite/authentication/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class RegistrationView(View):
+    def __init__(self):
+        pass
+
+    def get(self, request):
+        return render(request, 'authentication/register.html')
