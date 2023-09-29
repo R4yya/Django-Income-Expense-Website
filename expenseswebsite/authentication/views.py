24f1cdd126956728baad2c6fb4e5ea1f50fe9b7a
R4yya <121268707+R4yya@users.noreply.github.com>
2023-09-29 18:17:51 +0300

    Update views.py

--- expenseswebsite/authentication/views.py
+++ expenseswebsite/authentication/views.py
@@ -16,7 +16,28 @@ class RegistrationView(View):
         return render(request, 'authentication/register.html')
 
     def post(self, request):
-        messages.success(request, '1')
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+
+        context = {
+            'fieldValues': request.POST
+        }
+
+        if not User.objects.filter(username=username).exists():
+            if not User.objects.filter(email=email).exists():
+                if len(password) < 6:
+                    messages.error(request, 'Password is too short')
+                    return render(request, 'authentication/register.html', context=context)
+
+                user = User.objects.create_user(username=username, email=email)
+                user.set_password(password)
+                user.save()
+
+                messages.success(request, 'Account successfully created')
+
+                return render(request, 'authentication/register.html')
+
         return render(request, 'authentication/register.html')
 
 
