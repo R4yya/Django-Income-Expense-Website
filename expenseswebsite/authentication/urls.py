3ba720eee31290bc2c000308cbbe9cd477fcb771
R4yya <121268707+R4yya@users.noreply.github.com>
2023-09-29 12:42:35 +0300

    Update urls.py

--- expenseswebsite/authentication/urls.py
+++ expenseswebsite/authentication/urls.py
@@ -1,9 +1,10 @@
-from .views import RegistrationView, UsernameValidationView
+from .views import RegistrationView, UsernameValidationView, EmailValidationView
 
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path('register', RegistrationView.as_view(), name='register'),
-    path('validate-username', csrf_exempt(UsernameValidationView.as_view()), name='validate-username')
+    path('validate-username', csrf_exempt(UsernameValidationView.as_view()), name='validate-username'),
+    path('validate-email', csrf_exempt(EmailValidationView.as_view()), name='validate-email')
 ]
