3e3b4a967a5375ca2257c73e489aa102f704e805
R4yya <121268707+R4yya@users.noreply.github.com>
2023-09-30 15:48:05 +0300

    Update settings.py

--- expenseswebsite/expenseswebsite/settings.py
+++ expenseswebsite/expenseswebsite/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'expenses'
+    'expenses',
+    'userprefernces'
 ]
 
 MIDDLEWARE = [
