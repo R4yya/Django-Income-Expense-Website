065bee49bcf88f7b43a933c7b1fcc5aef8ae2f35
R4yya <121268707+R4yya@users.noreply.github.com>
2023-10-01 01:07:29 +0300

    Update models.py

--- expenseswebsite/expenses/models.py
+++ expenseswebsite/expenses/models.py
@@ -16,5 +16,12 @@ class Expense(models.Model):
     class Meta:
         ordering: ['-date']
 
+
 class Category(models.Model):
     name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
