7227ceba38b4909070b3af2a3bd14787362c8566
R4yya <121268707+R4yya@users.noreply.github.com>
2023-10-01 16:44:23 +0300

    Update views.py

--- expenseswebsite/expenses/views.py
+++ expenseswebsite/expenses/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from .models import Category, Expense
+
 
 @login_required(login_url='/authentication/login')
 def index(request):
+    categories = Category.objects.all()
     return render(request, 'expenses/index.html')
 
 
 def add_expense(request):
-    return render(request, 'expenses/add-expense.html')
+    categories = Category.objects.all()
+    context = {
+        'categories': categories
+    }
+    return render(request, 'expenses/add-expense.html', context)
