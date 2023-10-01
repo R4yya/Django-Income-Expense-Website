0cee39ddb69d77380309b065a681b76986c4def2
R4yya <121268707+R4yya@users.noreply.github.com>
2023-10-01 21:01:15 +0300

    Update urls.py

--- expenseswebsite/expenses/urls.py
+++ expenseswebsite/expenses/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='expenses'),
-    path('add-expense', views.add_expense, name='add-expense')
+    path('add-expense', views.add_expense, name='add-expense'),
+    path('edit-expense/<int:id>', views.edit_expense, name='edit-expense')
 ]
