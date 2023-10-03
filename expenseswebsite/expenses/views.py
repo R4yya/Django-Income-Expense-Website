eae91afcaa225faa81ee549253b64c82aa9f9db1
R4yya <121268707+R4yya@users.noreply.github.com>
2023-10-03 23:09:46 +0300

    Update views.py

--- expenseswebsite/expenses/views.py
+++ expenseswebsite/expenses/views.py
@@ -4,20 +4,22 @@ from django.contrib import messages
 from django.core.paginator import Paginator
 from django.http import JsonResponse
 
+from userpreferences.models import UserPreference
 from .models import Category, Expense
 from json import loads
 
 
 @login_required(login_url='/authentication/login')
 def index(request):
-    categories = Category.objects.all()
     expenses = Expense.objects.filter(owner=request.user)
     paginator = Paginator(expenses, 5)
     page_number = request.GET.get('page')
     page_obj = Paginator.get_page(paginator, page_number)
+    currency = UserPreference.objects.get(user=request.user).currency
     context = {
         'expenses': expenses,
-        'page_obj': page_obj
+        'page_obj': page_obj,
+        'currency': currency
     }
 
     return render(request, 'expenses/index.html', context)
