02a1dacadff3f83fb10edfe1fd31fa42b2859849
R4yya <121268707+R4yya@users.noreply.github.com>
2023-10-03 19:43:25 +0300

    Create search_expenses.js

---
+++ expenseswebsite/expenseswebsite/static/js/search_expenses.js
@@ -0,0 +1,48 @@
+const searchField = document.querySelector("#searchField");
+
+const tableOutput = document.querySelector(".table-output");
+tableOutput.style.display = "none";
+
+const appTable = document.querySelector(".app-table");
+
+const paginationContainer = document.querySelector(".pagination-container");
+
+const tableBody = document.querySelector(".table-body")
+
+searchField.addEventListener("keyup", (e) => {
+    const searchValue = e.target.value;
+
+    if (searchValue.trim().length > 0){
+        paginationContainer.style.display = "none";
+
+        tableBody.innerHTML = "";
+
+        fetch("/search-expense", {
+            body: JSON.stringify({searchText: searchValue}),
+            method: "POST"
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                appTable.style.display = "none";
+                tableOutput.style.display = "block";
+
+                if (data.length === 0){
+                    tableOutput.innerHTML = 'No results found'
+                } else {
+                    data.forEach((item) => {
+                        tableBody.innerHTML += `
+                            <tr>
+                            <td>${item.amount}</td>
+                            <td>${item.category}</td>
+                            <td>${item.description}</td>
+                            <td>${item.date}</td>
+                            </tr>`;
+                    });
+                }
+            });
+    } else {
+        tableOutput.style.display = "none";
+        appTable.style.display = "block";
+        paginationContainer.style.display = "block";
+    }
+});
