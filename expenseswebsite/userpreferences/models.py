3c66b2473714b275e221cb486579e6bb3db22135
R4yya <121268707+R4yya@users.noreply.github.com>
2023-10-03 23:09:43 +0300

    Update models.py

--- expenseswebsite/userpreferences/models.py
+++ expenseswebsite/userpreferences/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class UserPreference(models.Model):
-	user = models.OneToOneField(to=User, on_delete=models.CASCADE)
-	currency = models.CharField(max_length=255, blank=True, null=True)
 
-	def __str__(self):
-		return f'{str(user)}’s preferences'
+class UserPreference(models.Model):
+    user = models.OneToOneField(to=User, on_delete=models.CASCADE)
+    currency = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return f'{str(user)}’s preferences'
