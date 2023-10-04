0a2ef3f616edbc1cf7c9e72ae23a4dc38f0cc16f
R4yya <121268707+R4yya@users.noreply.github.com>
2023-10-04 22:28:08 +0300

    Update urls.py

--- expenseswebsite/authentication/urls.py
+++ expenseswebsite/authentication/urls.py
@@ -1,4 +1,4 @@
-from .views import RegistrationView, UsernameValidationView, EmailValidationView, VerificationView, LoginView, LogoutView
+from .views import RegistrationView, UsernameValidationView, EmailValidationView, VerificationView, LoginView, LogoutView, ResetPassword
 
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
@@ -9,5 +9,6 @@ urlpatterns = [
     path('logout', LogoutView.as_view(), name='logout'),
     path('validate-username', csrf_exempt(UsernameValidationView.as_view()), name='validate-username'),
     path('validate-email', csrf_exempt(EmailValidationView.as_view()), name='validate-email'),
-    path('activate/<uidb64>/<token>', VerificationView.as_view(), name='activate')
+    path('activate/<uidb64>/<token>', VerificationView.as_view(), name='activate'),
+    path('reset-password', ResetPassword.as_view(), name='reset-password'),
 ]
