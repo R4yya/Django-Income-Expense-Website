9206a55362f97044e69a7f92f5d345eca196b6d5
R4yya <121268707+R4yya@users.noreply.github.com>
2023-10-04 16:08:48 +0300

    Update search_income.js

---
+++ expenseswebsite/expenseswebsite/static/js/search_income.js
@@ -0,0 +1,52 @@
+const searchField = document.querySelector("#searchField");
+
+const tableOutput = document.querySelector(".table-output");
+tableOutput.style.display = "none";
+
+const appTable = document.querySelector(".app-table");
+
+const paginationContainer = document.querySelector(".pagination-container");
+
+const noResults = document.querySelector(".no-results");
+
+const tableBody = document.querySelector(".table-body");
+
+searchField.addEventListener("keyup", (e) => {
+    const searchValue = e.target.value;
+
+    if (searchValue.trim().length > 0){
+        paginationContainer.style.display = "none";
+
+        tableBody.innerHTML = "";
+
+        fetch("/search-income", {
+            body: JSON.stringify({searchText: searchValue}),
+            method: "POST"
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                appTable.style.display = "none";
+                tableOutput.style.display = "block";
+
+                if (data.length === 0){
+                    noResults.style.display = "block";
+                    tableOutput.style.display = "none";
+                } else {
+                    noResults.style.display = "none";
+                    data.forEach((item) => {
+                        tableBody.innerHTML += `
+                            <tr>
+                            <td>${item.amount}</td>
+                            <td>${item.source}</td>
+                            <td>${item.description}</td>
+                            <td>${item.date}</td>
+                            </tr>`;
+                    });
+                };
+            });
+    } else {
+        tableOutput.style.display = "none";
+        appTable.style.display = "block";
+        paginationContainer.style.display = "block";
+    };
+});
