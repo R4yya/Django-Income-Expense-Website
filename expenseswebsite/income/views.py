a47cb2d73578b283fd84593048a2a72cfbb53e9b
R4yya <121268707+R4yya@users.noreply.github.com>
2023-10-04 14:38:35 +0300

    Update views.py

--- expenseswebsite/income/views.py
+++ expenseswebsite/income/views.py
@@ -25,6 +25,21 @@ def index(request):
     return render(request, 'income/index.html', context)
 
 
+def search_income(request):
+    if request.method == 'POST':
+        search_string = loads(request.body).get('searchText')
+
+        income = Income.objects.filter(
+            amount__istartswith=search_string, owner=request.user) | Expense.objects.filter(
+            date__istartswith=search_string, owner=request.user) | Expense.objects.filter(
+            description__icontains=search_string, owner=request.user) | Expense.objects.filter(
+            source__icontains=search_string, owner=request.user)
+
+        data = income.values()
+
+        return JsonResponse(list(data), safe=False)
+
+
 @login_required(login_url='/authentication/login')
 def add_income(request):
     sources = Source.objects.all()
@@ -66,3 +81,57 @@ def add_income(request):
         messages.success(request, 'Income saved successfully')
 
         return redirect('income')
+
+
+@login_required(login_url='/authentication/login')
+def edit_income(request, id):
+    sources = Source.objects.all()
+    income = Income.objects.get(pk=id)
+    context = {
+        'income': income,
+        'values': income,
+        'categories': sources
+    }
+
+    if request.method == 'GET':
+        return render(request, 'income/edit-income.html', context)
+
+    elif request.method == 'POST':
+        amount = request.POST['amount']
+
+        if not amount:
+            messages.error(request, 'Amount is required')
+            return render(request, 'income/edit-income.html', context)
+
+        source = request.POST['source']
+        description = request.POST['description']
+        date = request.POST['expense_date']
+
+        if not description:
+            messages.error(request, 'Description is required')
+            return render(request, 'income/edit-income.html', context)
+
+        if not date:
+            messages.error(request, 'Date is required')
+            return render(request, 'income/edit-income.html', context)
+
+        income.owner = request.user
+        income.amount = amount
+        income.date = date
+        income.source = source
+        income.description = description
+
+        income.save()
+
+        messages.success(request, 'Income updated successfully')
+
+        return redirect('income')
+
+
+def delete_income(request, id):
+    income = Income.objects.get(pk=id)
+    income.delete()
+
+    messages.success(request, 'Income removed successfully')
+
+    return redirect('income')
