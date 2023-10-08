5ccc426cd861a170b43d4b9572557dc4e7210d4d
R4yya <121268707+R4yya@users.noreply.github.com>
2023-10-08 22:03:42 +0300

    Update models.py

--- expenseswebsite/userpreferences/models.py
+++ expenseswebsite/userpreferences/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class UserPreference(models.Model):
     user = models.OneToOneField(to=User, on_delete=models.CASCADE)
-    currency = models.CharField(max_length=255, blank=True, null=True)
+    currency = models.CharField(max_length=255, blank=True, null=True, default='EUR - Euro')
 
     def __str__(self):
         return f'{str(user)}’s preferences'
