9df5b9369bddabe58942f5ccd07bcad87f6eb8f3
R4yya <121268707+R4yya@users.noreply.github.com>
2023-10-09 01:01:37 +0300

    Update views.py

--- expenseswebsite/income/views.py
+++ expenseswebsite/income/views.py
@@ -7,6 +7,8 @@ from django.http import JsonResponse
 from userpreferences.models import UserPreference
 from .models import Source, Income
 from json import loads
+from datetime import date, timedelta
+import calendar
 
 
 @login_required(login_url='/authentication/login')
@@ -135,3 +137,123 @@ def delete_income(request, id):
     messages.success(request, 'Income removed successfully')
 
     return redirect('income')
+
+
+def income_week_summary(request):
+    todays_date = date.today()
+    current_week_start = todays_date - timedelta(days=todays_date.weekday())
+    current_week_end = current_week_start + timedelta(days=6)
+
+    income = Income.objects.filter(
+        owner=request.user,
+        date__gte=current_week_start,
+        date__lte=current_week_end
+    )
+
+    days_of_week = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    final_rep = {day: 0 for day in days_of_week}
+
+    for item in income:
+        day_of_week = item.date.strftime('%a')
+        final_rep[day_of_week] += item.amount
+
+    return JsonResponse({'income_week_data': final_rep}, safe=False)
+
+
+def income_month_summary(request):
+    todays_date = date.today()
+    first_day_of_current_month = todays_date.replace(day=1)
+    last_day_of_current_month = (todays_date.replace(day=1) + timedelta(days=32)).replace(day=1) - timedelta(days=1)
+
+    income = Income.objects.filter(
+        owner=request.user,
+        date__gte=first_day_of_current_month,
+        date__lte=last_day_of_current_month
+    )
+
+    num_days = last_day_of_current_month.day
+    final_rep = {str(i): 0 for i in range(1, num_days + 1)}
+
+    for item in income:
+        day = str(item.date.day)
+        final_rep[day] += item.amount
+
+    return JsonResponse({'income_month_data': final_rep}, safe=False)
+
+
+def income_year_summary(request):
+    todays_date = date.today()
+    first_day_of_current_year = todays_date.replace(month=1, day=1)
+    last_day_of_current_year = todays_date.replace(month=12, day=31)
+
+    income = Income.objects.filter(
+        owner=request.user,
+        date__gte=first_day_of_current_year,
+        date__lte=last_day_of_current_year
+    )
+
+    month_names = [calendar.month_name[i] for i in range(1, 13)]
+    final_rep = {month: 0 for month in month_names}
+
+    for item in income:
+        month_name = calendar.month_name[item.date.month]
+        final_rep[month_name] += item.amount
+
+    return JsonResponse({'income_year_data': final_rep}, safe=False)
+
+
+def income_card_summary(request):
+    def get_value(income):
+        count = 0
+        amount = 0
+
+        for item in income:
+            amount += item.amount
+            count += 1
+
+        return {'count': count, 'amount': amount}
+
+    todays_date = date.today()
+
+    current_week_start = todays_date - timedelta(days=todays_date.weekday())
+    current_week_end = current_week_start + timedelta(days=6)
+
+    first_day_of_current_month = todays_date.replace(day=1)
+    last_day_of_current_month = (todays_date.replace(day=1) + timedelta(days=32)).replace(day=1) - timedelta(days=1)
+
+    first_day_of_current_year = todays_date.replace(month=1, day=1)
+    last_day_of_current_year = todays_date.replace(month=12, day=31)
+
+    all_income = Income.objects.filter(
+        owner=request.user,
+        date__gte=first_day_of_current_year,
+        date__lte=last_day_of_current_year
+    )
+
+    today_income = all_income.filter(
+        date=todays_date
+    )
+
+    week_income = all_income.filter(
+        date__gte=current_week_start,
+        date__lte=current_week_end
+    )
+
+    month_income = all_income.filter(
+        date__gte=first_day_of_current_month,
+        date__lte=last_day_of_current_month
+    )
+
+    final_rep = {
+        'today': get_value(today_income),
+        'week': get_value(week_income),
+        'month': get_value(month_income),
+        'year': get_value(all_income),
+    }
+
+    return JsonResponse({'income_card_data': final_rep}, safe=False)
+
+
+@login_required(login_url='/authentication/login')
+def income_stats(request):
+    return render(request, 'income/income-stats.html')
