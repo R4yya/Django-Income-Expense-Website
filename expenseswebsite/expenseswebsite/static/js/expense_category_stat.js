06cc96141c2d32ae8f8725eb32689cc99e0b803c
R4yya <121268707+R4yya@users.noreply.github.com>
2023-10-08 22:03:50 +0300

    Update expense_category_stat.js

--- expenseswebsite/expenseswebsite/static/js/expense_category_stat.js
+++ expenseswebsite/expenseswebsite/static/js/expense_category_stat.js
@@ -1,7 +1,7 @@
 const renderChart = (data, labels) => {
-    const ctx = document.getElementById('myChart');
+    const ctx1 = document.getElementById('myChart1');
 
-    new Chart(ctx, {
+    new Chart(ctx1, {
         type: 'doughnut',
         data: {
             labels: labels,
@@ -22,7 +22,50 @@ const renderChart = (data, labels) => {
                     'rgba(255, 99, 132, 1)'
                 ],
                 borderWidth: 1,
-                hoverOffset: 25
+                hoverOffset: 10
+            }]
+        },
+        options: {
+            plugins: {
+                title: {
+                    display: true,
+                    text: 'Expenses per category'
+                },
+                subtitle: {
+                    display: true,
+                    text: 'Last 6 months',
+                    padding: {
+                        bottom: 10
+                    }
+                },
+            }
+        }
+    });
+
+    const ctx2 = document.getElementById('myChart2');
+
+    new Chart(ctx2, {
+        type: 'doughnut',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Amount',
+                data: data,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(99, 255, 132, 0.2)',
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(255, 99, 132, 1)'
+                ],
+                borderWidth: 1,
+                hoverOffset: 10
             }]
         },
         options: {
