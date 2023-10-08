244f853e971847bd8ef7a1e27b745066505d5349
R4yya <121268707+R4yya@users.noreply.github.com>
2023-10-09 00:29:23 +0300

    Update expense_long_term_stat.js

--- expenseswebsite/expenseswebsite/static/js/expense_long_term_stat.js
+++ expenseswebsite/expenseswebsite/static/js/expense_long_term_stat.js
@@ -1,3 +1,58 @@
+let delayed;
+
+const renderWeekChart = (data, labels) => {
+    const ctx1 = document.getElementById('thisWeekChart').getContext('2d');
+
+    new Chart(ctx1, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'This week',
+
+                data: data,
+
+                backgroundColor: [
+                    'rgba(4, 110, 72, 0.2)',
+                ],
+
+                borderColor: [
+                    'rgba(4, 110, 72, 1)',
+                ],
+                borderWidth: 1,
+
+                fill: 'start',
+
+                pointStyle: 'circle',
+                pointBackgroundColor:[
+                    'rgba(4, 110, 72, 1)'
+                ]
+            }]
+        },
+        options: {
+            tension: 0.15,
+            responsive: false,
+            animation: {
+                onComplete: () => {
+                    delayed = true;
+                },
+                delay: (context) => {
+                    let delay = 0;
+                    if (context.type === 'data' && context.mode === 'default' && !delayed) {
+                      delay = context.dataIndex * 100 + context.datasetIndex * 100;
+                    }
+                    return delay;
+                },
+            },
+            scales: {
+                y: {
+                    beginAtZero: true
+                },
+            }
+        }
+    });
+};
+
 const renderMonthChart = (data, labels) => {
     const ctx1 = document.getElementById('thisMonthChart').getContext('2d');
 
@@ -28,8 +83,20 @@ const renderMonthChart = (data, labels) => {
             }]
         },
         options: {
-            tension: 0.4,
+            tension: 0.15,
             responsive: false,
+            animation: {
+                onComplete: () => {
+                    delayed = true;
+                },
+                delay: (context) => {
+                    let delay = 0;
+                    if (context.type === 'data' && context.mode === 'default' && !delayed) {
+                      delay = context.dataIndex * 100 + context.datasetIndex * 100;
+                    }
+                    return delay;
+                },
+            },
             scales: {
                 y: {
                     beginAtZero: true
@@ -40,7 +107,7 @@ const renderMonthChart = (data, labels) => {
 };
 
 const renderYearChart = (data, labels) => {
-    const ctx1 = document.getElementById('thisYearChart');
+    const ctx1 = document.getElementById('thisYearChart').getContext('2d');
 
     new Chart(ctx1, {
         type: 'line',
@@ -69,7 +136,19 @@ const renderYearChart = (data, labels) => {
             }]
         },
         options: {
-            tension: 0.4,
+            tension: 0.15,
+            animation: {
+                onComplete: () => {
+                    delayed = true;
+                },
+                delay: (context) => {
+                    let delay = 0;
+                    if (context.type === 'data' && context.mode === 'default' && !delayed) {
+                      delay = context.dataIndex * 100 + context.datasetIndex * 100;
+                    }
+                    return delay;
+                },
+            },
             responsive: false,
             scales: {
                 y: {
@@ -91,7 +170,8 @@ const getChartData = () => {
             ];
 
             renderMonthChart(data, lables);
-    });
+        });
+
     fetch('expense-year-summary')
         .then(res=>res.json())
         .then(results => {
@@ -102,7 +182,19 @@ const getChartData = () => {
             ];
 
             renderYearChart(data, lables);
-    });
+        });
+
+    fetch('expense-week-summary')
+        .then(res=>res.json())
+        .then(results => {
+            const week_data = results.expense_week_data;
+            const [lables, data] = [
+                Object.keys(week_data),
+                Object.values(week_data)
+            ];
+
+            renderWeekChart(data, lables);
+        });
 };
 
 document.onload = getChartData();
