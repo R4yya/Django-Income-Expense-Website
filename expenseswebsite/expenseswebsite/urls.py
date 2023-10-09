9d585542aee85f16f113d42f3856fc511ff6604a
R4yya <121268707+R4yya@users.noreply.github.com>
2023-10-09 03:25:49 +0300

    Update urls.py

--- expenseswebsite/expenseswebsite/urls.py
+++ expenseswebsite/expenseswebsite/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('expenses.urls')),
+    path('expense/', include('expenses.urls')),
     path('income/', include('income.urls')),
     path('authentication/', include('authentication.urls')),
     path('preferences/', include('userpreferences.urls')),
