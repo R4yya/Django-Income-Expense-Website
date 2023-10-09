761be4511aa36239dd31855e984ef461bdf37912
R4yya <121268707+R4yya@users.noreply.github.com>
2023-10-09 15:56:56 +0300

    Update urls.py

--- expenseswebsite/dashboard/urls.py
+++ expenseswebsite/dashboard/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='dashboard'),
+    path('expense-category-summary', views.expense_category_summary, name='expense-category-summary'),
+    path('income-source-summary', views.income_source_summary, name='income-source-summary'),
+    path('comparative-stat', views.comparative_stat, name='comparative-stat')
 ]
