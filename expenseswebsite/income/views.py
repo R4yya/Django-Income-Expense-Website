e7b50aec074c4026034b87b5faf208a213fd3d97
R4yya <121268707+R4yya@users.noreply.github.com>
2023-10-09 03:02:55 +0300

    Update views.py

--- expenseswebsite/income/views.py
+++ expenseswebsite/income/views.py
@@ -139,6 +139,42 @@ def delete_income(request, id):
     return redirect('income')
 
 
+def income_source_summary(request):
+    def get_source(income):
+        return income.source
+
+    def get_value(source):
+        amount = 0
+
+        filtered_by_source = income.filter(source=source)
+
+        for item in filtered_by_source:
+            amount += item.amount
+
+        return amount
+
+    todays_date = date.today()
+    first_day_of_current_month = todays_date.replace(day=1)
+    last_day_of_last_month = first_day_of_current_month - timedelta(days=1)
+    first_day_of_last_month = last_day_of_last_month.replace(day=1)
+
+    income = Income.objects.filter(
+        owner=request.user,
+        date__gte=first_day_of_last_month,
+        date__lte=last_day_of_last_month
+    )
+
+    final_rep = {}
+
+    source_list = list(set(map(get_source, income)))
+
+    for item in income:
+        for source in source_list:
+            final_rep[source] = get_value(source)
+
+    return JsonResponse({'income_source_data': final_rep}, safe=False)
+
+
 def income_week_summary(request):
     todays_date = date.today()
     current_week_start = todays_date - timedelta(days=todays_date.weekday())
