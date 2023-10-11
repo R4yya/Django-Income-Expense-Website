edd126dac973e18ea2d2d94a4b2140a7561b34df
R4yya <121268707+R4yya@users.noreply.github.com>
2023-10-11 18:43:57 +0300

    Update comparative_stat.js

--- expenseswebsite/expenseswebsite/static/js/comparative_stat.js
+++ expenseswebsite/expenseswebsite/static/js/comparative_stat.js
@@ -1,100 +1,101 @@
 let delayed;
 
-const renderComparativeChart = (data_expenses, labels_expenses, data_income, lables_income) => {
-    const ctx = document.getElementById('comparativeChart').getContext('2d');
-
-    new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: labels_expenses,
-            datasets: [
-            {
-                label: 'Expenses',
-
-                data: data_expenses,
-
-                backgroundColor: [
-                    'rgba(4, 110, 72, 0.2)',
-                ],
-
-                borderColor: [
-                    'rgba(4, 110, 72, 1)',
-                ],
-                borderWidth: 1,
-                borderRadius: 5,
-
-                fill: 'start',
-
-                pointStyle: 'circle',
-                pointBackgroundColor:[
-                    'rgba(4, 110, 72, 1)'
-                ]
-            },
-            {
-                label: 'Income',
-
-                data: data_income,
-
-                backgroundColor: [
-                    'rgba(196, 92, 94, 0.2)',
-                ],
-
-                borderColor: [
-                    'rgba(196, 92, 94, 1)',
-                ],
-                borderWidth: 1,
-                borderRadius: 5,
-
-                fill: 'start',
-
-                pointStyle: 'circle',
-                pointBackgroundColor:[
-                    'rgba(4, 110, 72, 1)'
-                ]
-            },]
-        },
-        options: {
-            tension: 0.15,
-            responsive: false,
-            animation: {
-                onComplete: () => {
-                    delayed = true;
-                },
-                delay: (context) => {
-                    let delay = 0;
-                    if (context.type === 'data' && context.mode === 'default' && !delayed) {
-                      delay = context.dataIndex * 100 + context.datasetIndex * 100;
-                    }
-                    return delay;
-                },
-            },
-            scales: {
-                y: {
-                    beginAtZero: true
-                },
-            }
-        }
-    });
-};
-
-const getChartData = () => {
+const renderComparativeChart = () => {
     fetch('comparative-stat')
         .then(res => res.json())
         .then(results => {
-            const expenses_data = results.comparative_data.expenses;
-            const [lables_expenses, data_expenses] = [
-                Object.keys(expenses_data),
-                Object.values(expenses_data)
+            const expenses_to_compare_data = results.comparative_data.expenses;
+            const [lables_expenses_to_compare, data_expenses_to_compare] = [
+                Object.keys(expenses_to_compare_data),
+                Object.values(expenses_to_compare_data)
             ];
 
-            const income_data = results.comparative_data.income;
-            const [lables_income, data_income] = [
-                Object.keys(income_data),
-                Object.values(income_data)
+            const income_to_compare_data = results.comparative_data.income;
+            const [lables_income_to_compare, data_income_to_compare] = [
+                Object.keys(income_to_compare_data),
+                Object.values(income_to_compare_data)
             ];
 
-            renderComparativeChart(data_expenses, lables_expenses, data_income, lables_income);
+            const comparative_chart_data = {
+                labels: lables_expenses_to_compare,
+                datasets: [
+                {
+                    label: 'Expenses',
+
+                    data: data_expenses_to_compare,
+
+                    backgroundColor: [
+                        'rgba(4, 110, 72, 0.2)',
+                    ],
+
+                    borderColor: [
+                        'rgba(4, 110, 72, 1)',
+                    ],
+                    borderWidth: 1,
+                    borderRadius: 5,
+
+                    fill: 'start',
+
+                    pointStyle: 'circle',
+                    pointBackgroundColor:[
+                        'rgba(4, 110, 72, 1)'
+                    ]
+                },
+                {
+                    label: 'Income',
+
+                    data: data_income_to_compare,
+
+                    backgroundColor: [
+                        'rgba(196, 92, 94, 0.2)',
+                    ],
+
+                    borderColor: [
+                        'rgba(196, 92, 94, 1)',
+                    ],
+                    borderWidth: 1,
+                    borderRadius: 5,
+
+                    fill: 'start',
+
+                    pointStyle: 'circle',
+                    pointBackgroundColor:[
+                        'rgba(4, 110, 72, 1)'
+                    ]
+                }
+                ]
+            };
+
+            const comparative_chart_config = {
+                type: 'bar',
+                data: comparative_chart_data,
+                options: {
+                    responsive: false,
+                    animation: {
+                        onComplete: () => {
+                            delayed = true;
+                        },
+                        delay: (context) => {
+                            let delay = 0;
+                            if (context.type === 'data' && context.mode === 'default' && !delayed) {
+                              delay = context.dataIndex * 100 + context.datasetIndex * 100;
+                            }
+                            return delay;
+                        },
+                    },
+                    scales: {
+                        y: {
+                            beginAtZero: true
+                        },
+                    }
+                }
+            };
+
+            const comparativeChart = new Chart(
+                document.getElementById('comparativeChart'),
+                comparative_chart_config
+            );
         });
 };
 
-document.onload = getChartData();
+document.onload = renderComparativeChart();
