1a06e09786ebce7dea9fc5830c4e3a3d05662099
R4yya <121268707+R4yya@users.noreply.github.com>
2023-10-15 15:29:01 +0300

    Update urls.py

--- expenseswebsite/income/urls.py
+++ expenseswebsite/income/urls.py
@@ -10,7 +10,6 @@ urlpatterns = [
     path('delete-income/<int:id>', views.delete_income, name='delete-income'),
     path('search-income', csrf_exempt(views.search_income), name='search-income'),
     path('income-stats', views.income_stats, name='income-stats'),
-     path('income-source-summary', views.income_source_summary, name='income-source-summary'),
     path('income-card-summary', views.income_card_summary, name='income-card-summary'),
     path('income-week-summary', views.income_week_summary, name='income-week-summary'),
     path('income-month-summary', views.income_month_summary, name='income-month-summary'),
