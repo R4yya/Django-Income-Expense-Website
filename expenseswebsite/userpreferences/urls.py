221ebae906470465c45103ad5c03a903fa339f54
R4yya <121268707+R4yya@users.noreply.github.com>
2023-10-17 01:25:04 +0300

    Update urls.py

--- expenseswebsite/userpreferences/urls.py
+++ expenseswebsite/userpreferences/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='preferences')
+    path('', views.index, name='preferences'),
+    path('account', views.account_preferences, name='account'),
 ]
