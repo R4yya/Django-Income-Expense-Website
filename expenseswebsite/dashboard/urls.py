91c1b0c8309b60d2f66697b923006a1582db515e
R4yya <121268707+R4yya@users.noreply.github.com>
2023-10-16 23:42:28 +0300

    Update urls.py

--- expenseswebsite/dashboard/urls.py
+++ expenseswebsite/dashboard/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.index, name='dashboard'),
     path('dahsboard-expense-category-summary', views.expense_category_summary, name='dahsboard-expense-category-summary'),
     path('dahsboard-income-source-summary', views.income_source_summary, name='dahsboard-income-source-summary'),
-    path('comparative-stat', views.comparative_stat, name='comparative-stat')
+    path('comparative-stat-month', views.comparative_stat_month, name='comparative-stat-month'),
+    path('comparative-stat-year', views.comparative_stat_year, name='comparative-stat-year')
 ]
