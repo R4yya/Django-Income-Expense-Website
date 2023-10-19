58f5eae846950ebf14621bef223e9057aa00efe9
R4yya <121268707+R4yya@users.noreply.github.com>
2023-10-19 14:20:36 +0300

    Update views.py

--- expenseswebsite/expenses/views.py
+++ expenseswebsite/expenses/views.py
@@ -200,11 +200,11 @@ def expense_year_summary(request):
         date__lte=last_day_of_current_year
     )
 
-    month_names = [calendar.month_name[i] for i in range(1, 13)]
+    month_names = [calendar.month_abbr[i] for i in range(1, 13)]
     final_rep = {month: 0 for month in month_names}
 
     for expense in expenses:
-        month_name = calendar.month_name[expense.date.month]
+        month_name = calendar.month_abbr[expense.date.month]
         final_rep[month_name] += expense.amount
 
     return JsonResponse({'expense_year_data': final_rep}, safe=False)
