78d10b7d3b534a0f85e689cdc15b3e8c5a2b9881
R4yya <121268707+R4yya@users.noreply.github.com>
2023-10-29 20:16:03 +0300

    Update urls.py

--- expenseswebsite/userpreferences/urls.py
+++ expenseswebsite/userpreferences/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='preferences'),
     path('account', views.account_preferences, name='account'),
-    path('change_firstname', views.change_firstname, name='change_firstname')
+    path('change_firstname', views.change_firstname, name='change_firstname'),
+    path('change_lastname', views.change_lastname, name='change_lastname'),
+    path('delete_account', views.delete_account, name='delete_account')
 ]
